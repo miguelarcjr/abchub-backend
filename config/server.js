668c01cdbf9c23e3028c3920f4cdfc9a92537529
Miguel Junior <miguelarcjunior@gmail.com>
2023-10-04 06:21:57 -0400

    feat: ssl added

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://api-abchub.lowee.xyz:1337',
+  url: 'https://api-abchub.lowee.xyz',
   app: {
     keys: env.array('APP_KEYS'),
   },
